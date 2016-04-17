d0f8532251597e0280ac646465be2dc16fc1ef02
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-04-17 13:34:52 -0700

    Created base

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//Use the new proxy api
+var Proxy = require('harmony-proxy');
+
+//Create the handle function
+var handle = {
+
+  get: function(methods, meth) {
+    switch(meth) {
+      case '_get':
+        return methods
+
+      case 'run':
+        return (obj) => {
+          return {
+            obj,
+            results: methods.map((meth) => {
+              return obj[meth.name].apply(obj, meth.args)
+            })
+          }
+        }
+
+      default:
+        methods.push({name: meth})
+        return (() => {
+          var i = methods.length-1
+          return (...args) => {
+            methods[i].args = args
+          }
+        })()
+    }
+  },
+  set: function() {
+    throw new Error('Unable to set prop of LazyChain')
+  }
+}
+
+var LazyChain = function() {
+  return new Proxy([], handle)
+}
+
+module.exports = LazyChain;
---
+++ test/lazy-chaining.js
@@ -0,0 +1,10 @@
+const LazyChain = require('../index.js')
+
+describe('LazyChain', () => {
+
+  it('should load properly', () => {
+    new LazyChain()
+    LazyChain()
+  })
+
+})
